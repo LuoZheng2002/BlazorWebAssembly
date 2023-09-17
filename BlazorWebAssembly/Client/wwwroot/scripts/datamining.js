cf56f3a1130f3da92d510a8a301e2cbe3f3b6956
luozheng2002 <luoz@umich.edu>
2023-09-17 11:50:15 -0400

    hatsune_miku

---
+++ BlazorWebAssembly/Client/wwwroot/scripts/datamining.js
@@ -0,0 +1,38 @@
+﻿let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+let div = document.getElementById('div');
+let image = new Image();
+image.src = 'Images/Hatsune_Miku.png';
+
+
+
+// Set up the game loop
+const frameRate = 60; // Frames per second
+const frameDelay = 1000 / frameRate; // Delay between frames in milliseconds
+let lastFrameTime = 0;
+let x = 150;
+let y = 150;
+let accumulatedTime = 0;
+function gameLoop(timestamp) {
+    // Calculate the time since the last frame
+    const elapsedTime = timestamp - lastFrameTime;
+    accumulatedTime = accumulatedTime + elapsedTime;
+    // Only update and render the game if enough time has passed
+    if (elapsedTime >= frameDelay) {
+        // Clear the canvas
+        let realX = x + 150 * Math.sin(accumulatedTime/1000);
+        let realY = y + 150 * Math.sin(accumulatedTime * Math.PI/1000);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(image, realX, realY, 200, 200);
+        div.innerText = Number(div.innerText) + 1;
+
+        // Store the current timestamp as the last frame time
+        lastFrameTime = timestamp;
+    }
+
+    // Request the next frame
+    requestAnimationFrame(gameLoop);
+}
+
+
+requestAnimationFrame(gameLoop);
